6519684478fff1bba0755406be9f849dbad7a7ff
Damilare Oyedeji <fuglydami@gmail.com>
2021-03-30 22:40:22 +0100

    new commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,33 +1,14 @@
 import React from 'react'
 import { FeaturedProducts, Hero, Services, Contact } from '../components'
-import { motion } from 'framer-motion'
 
-const containerVariants = {
-  hidden: {
-    opacity: 0,
-  },
-  visible: {
-    opacity: 1,
-    transition: { delay: 1.5, duration: 1.5 },
-  },
-  exit: {
-    x: '-100vw',
-    transition: { ease: 'easeInOut' },
-  },
-}
 const HomePage = () => {
   return (
-    <motion.main
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-    >
+    <main>
       <Hero />
       <FeaturedProducts />
       <Services />
       <Contact />
-    </motion.main>
+    </main>
   )
 }
 
